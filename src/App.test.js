a4d70a0eef3114c67a2a58312a5e58e020196764
Matthew Serwer <Matthew.Serwer@gmail.com>
2020-10-29 18:20:09 -0400

    cleand up code

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
-import ContactForm from './components/ContactForm'
+
 
 test("renders App without crashing", () => {
   render(<App />);
